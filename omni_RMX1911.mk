b14b7904291ace7b49e96fcc21df5185f72c43b3
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-11-02 07:33:12 +0100

    RMX1911: move all props to system.prop
    - drop prebuilt libbinder
    - set platform device base flag

--- omni_RMX1911.mk
+++ omni_RMX1911.mk
@@ -38,21 +38,6 @@ PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX1911
 PRODUCT_MANUFACTURER := Realme
 
-#ADB
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.secure=0 \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    persist.sys.usb.config=adb,mtp \
-    sys.usb.config=adb,mtp
-
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.usb.controller=4e00000.dwc3 \
-    sys.usb.rndis.func.name=rndis_bam \
-    sys.usb.rmnet.func.name=rmnet_bam \
-    persist.sys.isUsbOtgEnabled=true \
-    vendor.gatekeeper.disable_spu=true
