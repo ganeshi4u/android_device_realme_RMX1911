8faa3b1a99aca6ddb684ffd8e30efacbc52ea1c8
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-20 14:09:21 +0000

    RMX1911: Build power hal from source

--- device.mk
+++ device.mk
@@ -256,8 +256,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-impl \
-    android.hardware.power@1.2-service
+    android.hardware.power@1.0 \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # QTI
 PRODUCT_COPY_FILES += \
