8f0dd7a8354626a87c05248d612ee2be36679232
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-21 22:19:58 +0530

    RMX1911: blobs: Raw vendor files list

--- extract-files.sh
+++ extract-files.sh
@@ -14,9 +14,9 @@ VENDOR=realme
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor_los-P/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -40,13 +40,13 @@ while [ "$1" != "" ]; do
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
-fi
-
 # Initialize the helper for common device
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+
+#BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+#sed -i "s|/vendor/lib/modules|/vendor/lib/modulesp|g" "$BLOB_ROOT"/vendor/lib/modulesp/modules.dep
 
 "$MY_DIR"/setup-makefiles.sh
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,9 +16,9 @@ INITIAL_COPYRIGHT_YEAR=2019
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor_los-P/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
